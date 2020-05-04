994ecba1875986e503dd90f76246bbdca962fd02
sampreeth <sampreeth555@gmail.com>
2020-05-04 10:56:47 +0530

    signon to heroku

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -13,7 +13,7 @@ router.get('/logout', (req, res) => {
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
-    res.send('success')
+    res.send('successfully logged in using google signon')
 });
 
 module.exports = router;
