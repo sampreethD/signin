5412e42a6e469d9f55ed74396d7c221ce15774a6
sampreeth <sampreeth555@gmail.com>
2020-05-04 11:59:06 +0530

    test

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -13,7 +13,7 @@ passport.use(
         //options for strategy
         clientID: keys.clientID,
         clientSecret: keys.clientSecret,
-        callbackURL: `http://127.0.0.1:${process.env.PORT}/auth/google/redirect`
+        callbackURL: `http://127.0.0.1:3000/auth/google/redirect`
     }, (accessToken, refreshToken, profile, cb) => {
         //callback from passport.
         console.log('user data can be found in profile', profile)
